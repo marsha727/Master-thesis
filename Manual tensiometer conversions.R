6538c9fd43d2b2bc9e85583fba4bb3c29d5a8c4a
marsha727 <marsharoverss@gmail.com>
2023-11-07 15:12:09 +0100

    Incorrect WFPS

--- Manual tensiometer conversions.R
+++ Manual tensiometer conversions.R
@@ -15,7 +15,6 @@ Tensiometer_cmH20 <- Tensiometer %>%
   mutate(across(.cols = 2:10, ~kPa_to_cmH2O(.))) #here filter for TMAP
 
 
-
 #non linear interpolation now dividing the use of end and begin depths
 #make a subset of the values i require (I remove NA row manually for now)
 Subset_Bodem_fysische_metingen <- Bodem_fysische_metingen %>% 
@@ -27,41 +26,6 @@ depth_to_interpolate <- 30 #depth that is missing
 Subset_Bodem_fysische_metingen$a <- as.numeric(Subset_Bodem_fysische_metingen$a)
 Subset_Bodem_fysische_metingen$begindiepte <- as.numeric(Subset_Bodem_fysische_metingen$begindiepte)
 Subset_Bodem_fysische_metingen$einddiepte <- as.numeric(Subset_Bodem_fysische_metingen$einddiepte)
-
-#Interpolate first on begin then end depth and take the average value as it is a range
-#WCS
-interpolated_WCS_b <- spline(Subset_Bodem_fysische_metingen$begindiepte, Subset_Bodem_fysische_metingen$WCS, xout = depth_to_interpolate)$y
-interpolated_WCS_e <- spline(Subset_Bodem_fysische_metingen$einddiepte, Subset_Bodem_fysische_metingen$WCS, xout = depth_to_interpolate)$y
-interpolated_WCS = mean(c(interpolated_WCS_b, interpolated_WCS_e))
-#a
-interpolated_a_b <- spline(Subset_Bodem_fysische_metingen$begindiepte, Subset_Bodem_fysische_metingen$a, xout = depth_to_interpolate)$y
-interpolated_a_e <- spline(Subset_Bodem_fysische_metingen$einddiepte, Subset_Bodem_fysische_metingen$a, xout = depth_to_interpolate)$y
-interpolated_a = mean(c(interpolated_a_b, interpolated_a_e))
-#n
-interpolated_n_b <- spline(Subset_Bodem_fysische_metingen$begindiepte, Subset_Bodem_fysische_metingen$n, xout = depth_to_interpolate)$y
-interpolated_n_e <- spline(Subset_Bodem_fysische_metingen$einddiepte, Subset_Bodem_fysische_metingen$n, xout = depth_to_interpolate)$y
-interpolated_n = mean(c(interpolated_n_b, interpolated_n_e))
-#m
-interpolated_m_b <- spline(Subset_Bodem_fysische_metingen$begindiepte, Subset_Bodem_fysische_metingen$m, xout = depth_to_interpolate)$y
-interpolated_m_e <- spline(Subset_Bodem_fysische_metingen$einddiepte, Subset_Bodem_fysische_metingen$m, xout = depth_to_interpolate)$y
-interpolated_m = mean(c(interpolated_m_b, interpolated_m_e))
-
-#Make a new row of the interpolated values to the subset in similar format
-new_row <- Subset_Bodem_fysische_metingen %>% 
-  summarize(
-    Locatie = "INT",
-    begindiepte = depth_to_interpolate,
-    einddiepte = depth_to_interpolate,
-    WCS = interpolated_WCS,
-    WCR = 0,
-    a = interpolated_a,
-    n = interpolated_n,
-    m = interpolated_m,
-    across(.cols = -c(Locatie, begindiepte, einddiepte, WCS, WCR, a, n, m), .fns = ~NA)
-  )
-
-#attach row to the subset
-Subset_Bodem_fysische_metingen <- rbind(Subset_Bodem_fysische_metingen, new_row)
 
 #calling MvG values manually because i couldnt get my loop to function
 WCS1 <- Subset_Bodem_fysische_metingen$WCS[1]
@@ -97,8 +61,23 @@ Tensiometer_SWC <- Tensiometer_cmH20 %>%
   mutate(MS_TMAP_8_D_040 = WCR2 + ((WCS2 - WCR2)/((1+abs(a2 * MS_TMAP_8_D_040)^n2)^m2))) %>%
   mutate(MS_TMAP_9_D_060 = WCR3 + ((WCS3 - WCR3)/((1+abs(a3 * MS_TMAP_9_D_060)^n3)^m3)))
 
+WFPS_tensiometer <- Tensiometer_SWC %>% 
+  mutate(MS_TMAP_1_D_020 = MS_TMAP_1_D_020 / WCS1) %>% 
+  mutate(MS_TMAP_2_D_030 = MS_TMAP_2_D_030 / WCS1) %>% 
+  mutate(MS_TMAP_3_D_050 = MS_TMAP_3_D_050 / WCS2) %>% 
+
+  mutate(MS_TMAP_4_D_020 = MS_TMAP_4_D_020 / WCS1) %>% 
+  mutate(MS_TMAP_5_D_030 = MS_TMAP_5_D_040 / WCS2) %>% 
+  mutate(MS_TMAP_6_D_050 = MS_TMAP_6_D_060 / WCS3) %>% 
+
+  mutate(MS_TMAP_7_D_020 = MS_TMAP_7_D_020 / WCS1) %>% 
+  mutate(MS_TMAP_8_D_030 = MS_TMAP_8_D_040 / WCS2) %>% 
+  mutate(MS_TMAP_9_D_050 = MS_TMAP_9_D_060 / WCS3)
+
+
 #Make sure datetime is correct format
 Tensiometer_SWC$TIMESTAMP <- as.POSIXct(Tensiometer_SWC$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
+WFPS_tensiometer$TIMESTAMP <- as.POSIXct(WFPS_tensiometer$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
 
 ggplot(Tensiometer_SWC, aes(x = TIMESTAMP)) +
   geom_point(aes(y = MS_TMAP_1_D_020, color = "20 cm"), size = 0.3) +
@@ -245,3 +224,130 @@ ggplot(Tensiometer_SWC, aes(x = TIMESTAMP)) +
   scale_x_datetime(
     labels = scales::date_format("%B")
   )
+
+#WFPS
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_1_D_020, color = "20 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_2_D_030, color = "30 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_3_D_050, color = "50 cm"), size = 0.3) +
+  labs(
+    title = "WFPS (%) from θ(ψ) (P1-3)",
+    x = "Datetime",
+    y = "WFPS (%)"
+  ) +
+  scale_color_manual(
+    values = c("20 cm" = "red", "30 cm" = "purple", "50 cm" = "blue"),
+    labels = c("20 cm", "30 cm", "50 cm"),
+    name = "Depth"
+  ) +
+  theme(
+    plot.title = element_text(size = 10)
+  )+
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_4_D_020, color = "20 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_5_D_040, color = "40 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_6_D_060, color = "60 cm"), size = 0.3) +
+  labs(
+    title = "WFPS (%) from θ(ψ) (P4-6)",
+    x = "Datetime",
+    y = "WFPS (%)"
+  ) +
+  scale_color_manual(
+    values = c("20 cm" = "red", "40 cm" = "purple", "60 cm" = "blue"),
+    labels = c("20 cm", "40 cm", "60 cm"),
+    name = "Depth"
+  ) +
+  theme(
+    plot.title = element_text(size = 10)
+  )+
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_7_D_020, color = "20 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_8_D_040, color = "40 cm"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_9_D_060, color = "60 cm"), size = 0.3) +
+  labs(
+    title = "WFPS (%) from θ(ψ) (P7-9)",
+    x = "Datetime",
+    y = "WFPS (%)"
+  ) +
+  scale_color_manual(
+    values = c("20 cm" = "red", "40 cm" = "purple", "60 cm" = "blue"),
+    labels = c("20 cm", "40 cm", "60 cm"),
+    name = "Depth"
+  ) +
+  theme(
+    plot.title = element_text(size = 10)
+  ) +
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_1_D_020, color = "Probe 1"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_4_D_020, color = "Probe 4"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_7_D_020, color = "Probe 7"), size = 0.3) +
+  labs(
+    title = "WFPS (%) at 20 cm depth",
+    x = "Datetime",
+    y = "WFPS (%)"
+  ) +
+  scale_color_manual(
+    values = c("Probe 1" = "red", "Probe 4" = "green", "Probe 7" = "blue"),
+    labels = c("Probe 1", "Probe 4", "Probe 7"),
+    name = "Probes"
+  )  +
+  theme(
+    plot.title = element_text(size = 12)
+  ) +
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_3_D_050, color = "Probe 3"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_5_D_040, color = "Probe 5"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_8_D_040, color = "Probe 8"), size = 0.3) +
+  labs(
+    title = "WFPS (%) at 40 cm depth",
+    x = "Datetime",
+    y = "WFPS (%)"
+  ) +
+  scale_color_manual(
+    values = c("Probe 3" = "green",  "Probe 5" = "red", "Probe 8" = "blue"),
+    labels = c("Probe 3",  "Probe 5", "Probe 8"),
+    name = "Probes"
+  )  +
+  theme(
+    plot.title = element_text(size = 12)
+  ) +
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
+ggplot(WFPS_tensiometer, aes(x = TIMESTAMP)) +
+  geom_line(aes(y = MS_TMAP_6_D_060, color = "Probe 6"), size = 0.3) +
+  geom_line(aes(y = MS_TMAP_9_D_060, color = "Probe 9"), size = 0.3) +
+  labs(
+    title = "WFPS (%) at 60 cm depth",
+    x = "Datetime",
+    y = "WFPS(%)"
+  ) +
+  scale_color_manual(
+    values = c("Probe 6" = "red", "Probe 9" = "blue"),
+    labels = c("Probe 6", "Probe 9"),
+    name = "Probes"
+  )  +
+  theme(
+    plot.title = element_text(size = 12)
+  ) +
+  scale_x_datetime(
+    labels = scales::date_format("%B")
+  )
+
