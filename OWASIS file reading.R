dd482bb68fe2932d55b1a239f8655c38725c481f
marsha727 <marsharoverss@gmail.com>
2023-11-08 12:05:53 +0100

    Functional

--- OWASIS file reading.R
+++ OWASIS file reading.R
@@ -1,9 +1,10 @@
 library(terra)
-library(tidyverse)
+library(stringr)
+#do not load the tidyverse packages because that has conflicting functions with terra package
 
 #This sets the paths where the data should be retrieved. The first one is just a folder for raster files
 #from OWASIS and the second one is a shapefile of footprint of EC towers
-pathOWASISRast <-  "D:/R_master_thesis/Github/Master-thesis/Datasets/OWASIS/TIFF/groenehart"
+pathOWASISRast <-  "D:/R_master_thesis/Github/Master-thesis/Datasets/OWASIS/TIFF/groenehart/"
 pathTowers <- "D:/R_master_thesis/Github/Master-thesis/Datasets/OWASIS/Shapefiles/LAW_MS_ICOS_Tiwer_Path.shp"
 
 #It creates list of file paths to .tif files in a new list
@@ -33,7 +34,7 @@ df.owasis <- data.frame(day=lDay)
 #is temporatly taken by NTi, and for each NTi a new column is creates
 #is used to create a new column in the new dataframe df.owasis, with all its cells having NA
 for(NTi in pTowers[["Name"]]){
-    df.owasis[,NTi] <- NA
+  df.owasis[,NTi] <- NA
 }
 
 #It makes a new list
@@ -46,63 +47,47 @@ l.df.owasis[["BV"]] <- df.owasis
 #Another loop i think the point is that it needs to remove some parts of the of the file directory to 
 #find the directly later
 for(owi in lOWASIS){
-    
+  
   #the str_split splil text whenever a underscore is present. [[1]] then selects the first element from this split
   #The tail(...., 3) part extracts the last 3 elements from list created????
   #Then the [[1]] again selects the first part of that list
   #Remove Owasis from the string that is created
-    nvi <- str_remove(tail(str_split(owi,"_")[[1]],3)[[1]],"Owasis.")
-    #???
-    nvi1 <- OWASISAb[[nvi]][["Abbreviation"]]
-    #This part extracts the date from owi and assures its format
-    #Owi is from lOWASIS and this is the filepath?
-    dayi <- as.Date(str_split(tail(str_split(owi,"_")[[1]],2)[[1]],"T")[[1]][[1]],"%Y-%m-%d")
-    print(dayi)
+  nvi <- str_remove(tail(str_split(owi,"_")[[1]],3)[[1]],"Owasis.")
+  #???
+  nvi1 <- OWASISAb[[nvi]][["Abbreviation"]]
+  #This part extracts the date from owi and assures its format
+  #Owi is from lOWASIS and this is the filepath?
+  dayi <- as.Date(str_split(tail(str_split(owi,"_")[[1]],2)[[1]],"T")[[1]][[1]],"%Y-%m-%d")
+  print(dayi)
+  
+  #Check if any day is equal to date list created earlier
+  if(any(lDay==dayi)){
+    #this reads the raster file, in this case it seems that owi is part of a filename that i want to read
+    #whereas the pathOWASISRast is the directory
+    ri <- rast(paste0(pathOWASISRast,owi))
+    #ri contains values for locations, and pTowers are also spatial points
+    #the function tries to match those locations and extracts those values for those locations
+    #fun= function(x)meadian(x, na.rm=T) calculates the median of the values from each location
+    dfi <-extract(ri,pTowers,fun=function(x)median(x,na.rm=T)) 
     
-    #Check if any day is equal to date list created earlier
-    if(any(lDay==dayi)){
-      #this reads the raster file, in this case it seems that owi is part of a filename that i want to read
-      #whereas the pathOWASISRast is the directory
-        ri <- rast(paste0(pathOWASISRast,owi))
-      #ri contains values for locations, and pTowers are also spatial points
-      #the function tries to match those locations and extracts those values for those locations
-      #fun= function(x)meadian(x, na.rm=T) calculates the median of the values from each location
-        dfi <-extract(ri,pTowers,fun=function(x)median(x,na.rm=T)) 
-        
-        #I think this part attaches the values of dfi for each day to the the list that contains BBB etc.
-        for(i in seq.int(nrow(dfi))){
-            if(is.finite(dfi[i,ncol(dfi)])){
-                l.df.owasis[[nvi1]][which(lDay==dayi),pTowers[["Name"]][i,"Name"]] <- dfi[i,ncol(dfi)]            
-            }
-        }
-        
+    #I think this part attaches the values of dfi for each day to the the list that contains BBB etc.
+    for(i in seq.int(nrow(dfi))){
+      if(is.finite(dfi[i,ncol(dfi)])){
+        l.df.owasis[[nvi1]][which(lDay==dayi),pTowers[["Name"]][i,"Name"]] <- dfi[i,ncol(dfi)]            
+      }
     }
+    
+  }
 } 
 
 
 #I would like to get my data in the list to a dataframe for camparison later
 OWASIS_BBB <- data.frame(l.df.owasis[["BBB"]])
 
+start_date <- "2022-04-02"
+end_date <- "2022-07-26"
+
 #make sure the selection aligns of the dates
 OWASIS_BBB <- OWASIS_BBB %>% 
   filter(day >= start_date & day <= end_date)
-
-#need to make sure timestamp is also called datetime for later allignment
-OWASIS_BBB <- OWASIS_BBB %>% 
-  rename(datetime = day)
-
-Combined_SM_OWASIS <- inner_join(Combined_SM, OWASIS_BBB, by = "datetime")
-
-
-#plot for the whole year comparing the two tools without averages
-ggplot(Combined_SM_OWASIS) +
-  geom_line(mapping = aes(x = datetime, y = SM1, color = "Tensio"), size = 0.3) +
-  geom_line(mapping = aes(x = datetime, y = SWC_1_015, color = "Sentek015"), size = 0.3) +
-  geom_line(mapping = aes(x = datetime, y = LAW_MS, color = "OWASIS"), size = 0.3) +
-  labs (
-    title = "Soil moisture measurement tool comparison",
-    x = "Date",
-    y = "Soil moisture",
-  ) +
-  scale_color_manual(values = c("Tensio" = "red", "Sentek015" = "blue", "OWASIS" = "cadetblue"))
 
