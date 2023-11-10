d0753ead4563afcb0b677e495b6d8ca901a80e75
marsha727 <marsharoverss@gmail.com>
2023-11-10 10:05:24 +0100

    read files

---
+++ SWC WFPS dynamics.R
@@ -0,0 +1,4 @@
+SENTEK_all <- read.csv2("Transformed/Langeweide_Sentek.csv")
+TENSIO_SWC <- read.csv2("Transformed/Langeweide_Tensio.csv")
+OWASIS_BBB <- read.csv2("Transformed/Langeweide_OWASIS_BBB.csv")
+
