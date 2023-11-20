c5731bc602eb33480fb4d5c09c66770e71b5f13f
marsha727 <marsharoverss@gmail.com>
2023-11-20 17:28:39 +0100

    Check NaN in TENSIO

--- AFPS integration.R
+++ AFPS integration.R
@@ -6,7 +6,7 @@ OWASIS_BBB <- read.csv2("Transformed/Langeweide_OWASIS_BBB.csv")
 
 AFPS_SENTEK$datetime <- as.POSIXct(AFPS_SENTEK$datetime, format = "%Y-%m-%d %H:%M:%S")
 AFPS_TENSIO$TIMESTAMP <- as.POSIXct(AFPS_TENSIO$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
-OWASIS_BBB$Date <- as.POSIXct(OWASIS_BBB$Date, format = "%Y-%m-%d %H:%M:%S")
+OWASIS_BBB$Date <- as.POSIXct(OWASIS_BBB$Date, format = "%Y-%m-%d")
 
 #Fist check max depth of OWASIS_BBB in cm
 Max_GW_OWASIS <- abs(min(OWASIS_BBB$MedianGW_mmv)*100)
@@ -19,4 +19,43 @@ AFPS_int_SENTEK <- AFPS_SENTEK %>%
   mutate(Probe3= SWC_3_015 + SWC_3_025 + SWC_3_035 + SWC_3_045 + SWC_3_055)
 
 AFPS_int_TENSIO <- AFPS_TENSIO %>% 
-  mutate(Probe123 = MS_TMAP_1_D_020 + MS_TMAP_2_D_030 + MS_TMAP_3_D_050)
+  mutate(Probe123 = MS_TMAP_1_D_020 + MS_TMAP_2_D_030 + MS_TMAP_3_D_050) %>% 
+  mutate(Probe456 = MS_TMAP_4_D_020 + MS_TMAP_5_D_040 + MS_TMAP_6_D_060) %>% 
+  mutate(Probe789 = MS_TMAP_7_D_020 + MS_TMAP_8_D_040 + MS_TMAP_9_D_060)
+
+start_date <- "2022-04-02"
+end_date <- "2022-11-02"
+
+#filter for the dates
+AFPS_int_SENTEK <- AFPS_int_SENTEK %>% 
+  filter(datetime >= start_date & datetime <= end_date)
+
+AFPS_int_TENSIO <- AFPS_int_TENSIO %>% 
+  filter(TIMESTAMP >= start_date & TIMESTAMP <= end_date)
+
+#need to aggregate by day for OWASIS
+AFPS_int_SENTEK <- AFPS_int_SENTEK %>% 
+  group_by(datetime = format(datetime, "%Y-%m-%d")) %>% 
+  summarise(across(everything(), mean, na.rm = TRUE))
+
+#need to aggregate by day for OWASIS
+AFPS_int_TENSIO <- AFPS_int_TENSIO %>% 
+  group_by(datetime = format(TIMESTAMP, "%Y-%m-%d")) %>% 
+  summarise(across(everything(), mean, na.rm = TRUE))
+
+AFPS_int_SENTEK$datetime <- as.POSIXct(AFPS_int_SENTEK$datetime, format = "%Y-%m-%d")
+AFPS_int_TENSIO$TIMESTAMP <- as.POSIXct(AFPS_int_TENSIO$TIMESTAMP, format = "%Y-%m-%d")
+OWASIS_BBB$Date <- as.POSIXct(OWASIS_BBB$Date, format = "%Y-%m-%d")
+
+ggplot(AFPS_int_SENTEK) +
+  geom_line(aes(x = datetime, y = Probe1))
+
+ggplot(AFPS_int_TENSIO) +
+  geom_line(aes(x = TIMESTAMP, y = Probe789))
+
+ggplot(OWASIS_BBB) +
+  geom_line(aes(x = Date, y = MedianBBB))
+
+
+
+
