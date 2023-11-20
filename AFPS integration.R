b0597f78ccdd17168f61f100c7d893517672340c
marsha727 <marsharoverss@gmail.com>
2023-11-20 17:07:02 +0100

    End of day

--- AFPS integration.R
+++ AFPS integration.R
@@ -15,10 +15,8 @@ Max_GW_OWASIS <- abs(min(OWASIS_BBB$MedianGW_mmv)*100)
 
 #Integrate SENTEK
 AFPS_int_SENTEK <- AFPS_SENTEK %>% 
-  mutate(Probe1 = SWC_1_005 + SWC_1_015 + SWC_1_025 + SWC_1_035 + SWC_1_045 + SWC_1_055 + SWC_1_065 + SWC_1_075 + SWC_1_085 + SWC_1_095 +
-           SWC_1_105 + SWC_1_115) %>% 
-  mutate(Probe3= SWC_3_005 + SWC_3_015 + SWC_3_025 + SWC_3_035 + SWC_3_045 + SWC_3_055 + SWC_3_065 + SWC_3_075 + SWC_3_085 + SWC_3_095 +
-           SWC_3_105 + SWC_3_115)
+  mutate(Probe1 = SWC_1_015 + SWC_1_025 + SWC_1_035 + SWC_1_045 + SWC_1_055) %>% 
+  mutate(Probe3= SWC_3_015 + SWC_3_025 + SWC_3_035 + SWC_3_045 + SWC_3_055)
 
 AFPS_int_TENSIO <- AFPS_TENSIO %>% 
-  mutate(Probe)
+  mutate(Probe123 = MS_TMAP_1_D_020 + MS_TMAP_2_D_030 + MS_TMAP_3_D_050)
