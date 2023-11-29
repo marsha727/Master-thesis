562b4019f437e3056c2ad3846d94a2b9db02a40f
marsha727 <marsharoverss@gmail.com>
2023-11-29 13:39:35 +0100

    Correction gap filling and new interp method unfinished

--- AFPS tensio interpolation.R
+++ AFPS tensio interpolation.R
@@ -1,15 +1,24 @@
 #Subscript for tensiometer interpolation
 
-AFPS_TENSIO <- read.csv2("Transformed/Langeweide_Tensio_AFPS_mm_200.csv")
+library(tidyverse)
 
-AFPS_TENSIO$TIMESTAMP <- as.POSIXct(AFPS_TENSIO$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
+tensio <- read.csv("Datasets/LAW_TENS_2020-2023_clean.csv")
+
+tensio$TIMESTAMP <- as.POSIXct(tensio$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
+
+tensio <- tensio %>% 
+  select(TIMESTAMP, MS_TMAP_4_D_020, MS_TMAP_5_D_040, MS_TMAP_6_D_060,
+         MS_TMAP_7_D_020, MS_TMAP_8_D_040, MS_TMAP_9_D_060)
 
 #Dealing with missing values
-cor <- cor(AFPS_TENSIO$MS_TMAP_4_D_020, AFPS_TENSIO$MS_TMAP_7_D_020, method = "pearson", use = "complete.obs")
-cor2 <- cor(AFPS_TENSIO$MS_TMAP_5_D_040, AFPS_TENSIO$MS_TMAP_8_D_040, method = "pearson", use = "complete.obs")
+cor <- cor(tensio$MS_TMAP_4_D_020, tensio$MS_TMAP_7_D_020, method = "pearson", use = "complete.obs")
+cor2 <- cor(tensio$MS_TMAP_5_D_040, tensio$MS_TMAP_8_D_040, method = "pearson", use = "complete.obs")
 
-missing_values_indices <- which(is.na(AFPS_TENSIO$MS_TMAP_4_D_020))
-missing_values_indices2 <- which(is.na(AFPS_TENSIO$MS_TMAP_8_D_040))
+per <- mean(tensio$MS_TMAP_4_D_020 / tensio$MS_TMAP_7_D_020, na.rm = TRUE)
+per2 <- mean(tensio$MS_TMAP_5_D_040 / tensio$MS_TMAP_8_D_040, na.rm = TRUE)
+
+missing_values_indices <- which(is.na(tensio$MS_TMAP_4_D_020))
+missing_values_indices2 <- which(is.na(tensio$MS_TMAP_8_D_040))
 
 # Automatically find the start and end indices of the gap
 start_index <- min(missing_values_indices)
@@ -19,14 +28,14 @@ start_index2 <- min(missing_values_indices2)
 end_index2 <- max(missing_values_indices2)
 
 # Use the correlation to estimate missing values of MS_TMAP_4_D_020 based on MS_TMAP_7_D_020
-AFPS_TENSIO$MS_TMAP_4_D_020[start_index:end_index] <-
-  AFPS_TENSIO$MS_TMAP_7_D_020[start_index:end_index] * cor
+tensio$MS_TMAP_4_D_020[start_index:end_index] <-
+  tensio$MS_TMAP_7_D_020[start_index:end_index] * per
 
-AFPS_TENSIO$MS_TMAP_8_D_040[start_index2:end_index2] <-
-  AFPS_TENSIO$MS_TMAP_5_D_040[start_index2:end_index2] * cor2
+tensio$MS_TMAP_8_D_040[start_index2:end_index2] <-
+  tensio$MS_TMAP_5_D_040[start_index2:end_index2] * per2
 
 #Interpolation of tensiometer
-Depths_to_interpolate <- seq(20, 60, by = 1)
+{Depths_to_interpolate <- seq(20, 60, by = 1)
 
 model_data <- AFPS_TENSIO %>% 
   select(TIMESTAMP, MS_TMAP_4_D_020, MS_TMAP_5_D_040, MS_TMAP_6_D_060) %>% 
@@ -43,5 +52,37 @@ interp_data <- expand.grid(Depth = Depths_to_interpolate, Time = unique(model_da
 interp_data$InteractionTerm <- interaction(interp_data$Depth, interp_data$Time)
 
 interp_data$Value <- predict(model, newdata = interp_data)
-  
+} 
 
+#new options
+tensio_2 <- tensio %>% 
+  select(TIMESTAMP, MS_TMAP_4_D_020, MS_TMAP_5_D_040, MS_TMAP_6_D_060) %>% 
+  rename(Depth_20 = MS_TMAP_4_D_020, Depth_40 = MS_TMAP_5_D_040, Depth_60 = MS_TMAP_6_D_060)
+
+#create a new tensio file that sorts all SMP values in one column and a depth
+tensio_long <- tensio_2 %>% 
+  pivot_longer(cols = -TIMESTAMP,
+               names_to = "Column",
+               values_to = "SoilMatrixPotential") %>% 
+  mutate(Depth = str_extract(Column, "\\d+$")) %>% 
+  select(-Column)
+
+tensio_long$Depth <- as.numeric(tensio_long$Depth)
+
+#linear model for soil matrix potential and depth
+lm_depth_MP <- lm(SoilMatrixPotential ~ Depth + TIMESTAMP, data = tensio_long )
+
+Depths_to_interpolate <- seq(20, 60, by = 1)
+Times_to_interpolate <- unique(tensio_long$TIMESTAMP)
+
+new_data <- expand.grid(Depth = Depths_to_interpolate, TIMESTAMP = Times_to_interpolate)
+
+predict_values <- predict(lm_depth_MP, newdata = new_data)
+
+predicted_tensio <- data.frame(
+  Depth = rep(Depths_to_interpolate, each = length(Times_to_interpolate)),
+  TIMESTAMP = rep(Times_to_interpolate, each = length(Depths_to_interpolate)),
+  SoilMatrixPotential = predict_values
+)
+
+predicted_tensio$TIMESTAMP <- as.POSIXct(predicted_tensio$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
