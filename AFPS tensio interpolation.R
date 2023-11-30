290b58513678db1d0a0ce2fa5bc3e2da62d4a89f
marsha727 <marsharoverss@gmail.com>
2023-11-30 16:57:02 +0100

    AFPS conversions

--- AFPS tensio interpolation.R
+++ AFPS tensio interpolation.R
@@ -1,9 +1,11 @@
 #Subscript for tensiometer interpolation
 
 library(tidyverse)
-library(purrr)
+library(readxl)
 
 tensio <- read.csv("Datasets/LAW_TENS_2020-2023_clean.csv")
+Subset_Bodem_fysische_metingen <- read.csv2("MvG_Bodem_fysische_metingen.csv")
+
 
 tensio$TIMESTAMP <- as.POSIXct(tensio$TIMESTAMP, format = "%Y-%m-%d %H:%M:%S")
 
@@ -211,7 +213,7 @@ tensio_interp3 <- tensio_long3 %>%
   })
 
 
-#filter for combination
+#filter for combination of datasets
 tensio_interp2 <- tensio_interp2 %>% 
   select(date, depth, value.interp) %>% 
   rename(date_2 = date, depth_2 = depth, SMP_2 = value.interp)
@@ -223,6 +225,90 @@ tensio_interp3 <- tensio_interp3 %>%
 
 #combine datasets
 combine_tensio <- bind_cols(tensio_interp2, tensio_interp3)
+combine_tensio <- as.data.frame(combine_tensio)
 
 #Now perform the conversions
+kPa_to_cmH2O <- function(x){
+  ifelse(is.na(x), NA, x*10.1971623)
+}
+
+Tensiometer_cmH20 <- combine_tensio %>% 
+  mutate(across(contains("SMP"), ~kPa_to_cmH2O(.)))
+
+#calling MvG values manually because i couldnt get my loop to function
+WCS1 <- Subset_Bodem_fysische_metingen$WCS[1]
+WCR1 <- Subset_Bodem_fysische_metingen$WCR[1]
+a1 <- Subset_Bodem_fysische_metingen$a[1]
+n1 <- Subset_Bodem_fysische_metingen$n[1]
+m1 <- Subset_Bodem_fysische_metingen$m[1]
+
+WCS2 <- Subset_Bodem_fysische_metingen$WCS[2]
+WCR2 <- Subset_Bodem_fysische_metingen$WCR[2]
+a2 <- Subset_Bodem_fysische_metingen$a[2]
+n2 <- Subset_Bodem_fysische_metingen$n[2]
+m2 <- Subset_Bodem_fysische_metingen$m[2]
+
+WCS3 <- Subset_Bodem_fysische_metingen$WCS[3]
+WCR3 <- Subset_Bodem_fysische_metingen$WCR[3]
+a3 <- Subset_Bodem_fysische_metingen$a[3]
+n3 <- Subset_Bodem_fysische_metingen$n[3]
+m3 <- Subset_Bodem_fysische_metingen$m[3]
+
+#apply MvG function adjusted for interp depths
+SWC_TENSIO <- Tensiometer_cmH20 %>% 
+  mutate(
+    SWC2 = case_when(
+      between(depth_2, 15, 35) ~ WCR1 + (WCS1 - WCR1)/((1 + abs(a1 * SMP_2)^n1)^m1),
+      between(depth_2, 36, 55) ~ WCR2 + (WCS2 - WCR2)/((1 + abs(a2 * SMP_2)^n2)^m2),
+      between(depth_2, 56, 65) ~ WCR3 + (WCS3 - WCR3)/((1 + abs(a3 * SMP_2)^n3)^m3),
+      TRUE ~ NA_real_
+      ),
+    SWC3 = case_when(
+      between(depth_3, 15, 35) ~ WCR1 + (WCS1 - WCR1)/((1 + abs(a1 * SMP_3)^n1)^m1),
+      between(depth_3, 36, 55) ~ WCR2 + (WCS2 - WCR2)/((1 + abs(a2 * SMP_3)^n2)^m2),
+      between(depth_3, 56, 65) ~ WCR3 + (WCS3 - WCR3)/((1 + abs(a3 * SMP_3)^n3)^m3),
+      TRUE ~ NA_real_
+    )
+  )
+
+#Now convert to AFPS (%)
+AFPS_tensio <- SWC_TENSIO %>% 
+  mutate(
+    AFPS2 = case_when(
+      between(depth_2, 15, 35) ~ 1 - (SWC2 / WCS1),
+      between(depth_2, 36, 55) ~ 1 - (SWC2 / WCS2),
+      between(depth_2, 56, 65) ~ 1 - (SWC2 / WCS3),
+      TRUE ~ NA_real_
+    ),
+    AFPS3 = case_when(
+      between(depth_3, 15, 35) ~ 1 - (SWC3 / WCS1),
+      between(depth_3, 36, 55) ~ 1 - (SWC3 / WCS2),
+      between(depth_3, 56, 65) ~ 1 - (SWC3 / WCS3),
+      TRUE ~ NA_real_
+    )
+  )
+
+#Convert AFPS to mm
+AFPS_mm_tensio <- AFPS_tensio %>% 
+  mutate(
+    AFPS2_mm = case_when(
+      between(depth_2, 15, 35) ~ AFPS2 * WCS1 * 1,
+      between(depth_2, 36, 55) ~ AFPS2 * WCS2 * 1,
+      between(depth_2, 56, 65) ~ AFPS2 * WCS3 * 1,
+      TRUE ~ NA_real_
+    ),
+    AFPS3_mm = case_when(
+      between(depth_3, 15, 35) ~ AFPS3 * WCS1 * 1,
+      between(depth_3, 36, 55) ~ AFPS3 * WCS2 * 1,
+      between(depth_3, 56, 65) ~ AFPS3 * WCS3 * 1,
+      TRUE ~ NA_real_   
+    )
+  )
+
+
+
+
+
+
+
 
