77cf528ef7ce2f8f6bc27921112805e365e8d6eb
marsha727 <marsharoverss@gmail.com>
2023-12-07 12:20:29 +0100

    Cleaned up file

--- AFPS integration.R
+++ AFPS integration.R
@@ -20,13 +20,11 @@ AFPS_int_SENTEK <- AFPS_SENTEK %>%
 
 AFPS_int_TENSIO <- AFPS_TENSIO %>% 
   group_by(datetime) %>% 
-  summarise(AFPS_int2 = sum(AFPS2_mm), AFPS_int3 = sum(AFPS3_mm))
+  summarise(AFPS2 = sum(AFPS2), AFPS3 = sum(AFPS3))
 
-#Second selection for OWASIS
-AFPS_int_SENTEK_O <- AFPS_SENTEK %>% 
-  mutate(Probe1 = SWC_1_005 + SWC_1_015 + SWC_1_025) %>% 
-  mutate(Probe3 = SWC_3_005 + SWC_3_015 + SWC_3_025)
-
+#write a RDS just for APP
+write_rds(AFPS_int_SENTEK, "App/Langeweide_sentek_int.rds")
+write_rds(AFPS_int_TENSIO, "App/Langeweide_tensio_int.rds")
 
 #Period of analysis
 start_date <- "2022-04-02"
@@ -41,9 +39,6 @@ AFPS_int_SENTEK <- AFPS_int_SENTEK %>%
 AFPS_int_TENSIO <- AFPS_int_TENSIO %>% 
   filter(datetime >= start_date & datetime <= end_date2)
 
-AFPS_int_SENTEK_O <- AFPS_int_SENTEK_O %>% 
-  filter(datetime >= start_date & datetime <= end_date3)
-
 AFPS_SENTEK <- AFPS_SENTEK %>% 
   filter(datetime >= start_date & datetime <= end_date3)
 
@@ -56,24 +51,6 @@ Langeweide_data <- Langeweide_data %>%
 OWASIS_BBB <- OWASIS_BBB %>% 
   filter(Date >= start_date & Date <= end_date3)
 
-#GWL side project
-{#Extract GWL
-GWL <- Langeweide_data %>% 
-  select(datetime, WL_1, WL_2, WL_3, WL_4, WL_5, WL, WL_cor)
-
-#need to do an estimation on the conversion to NAP
-columns_to_mean <- c("WL_1", "WL_2", "WL_3", "WL_4", "WL_5")
-GWL_mmv <- GWL %>% 
-  mutate(WL_1 = WL_1 + 204) %>% 
-  mutate(WL_2 = WL_2 + 204) %>% 
-  mutate(WL_3 = WL_3 + 204) %>% 
-  mutate(WL_4 = WL_4 + 204) %>% 
-  mutate(WL_5 = WL_5 + 204)
-
-GWL_mmv <- GWL_mmv %>% 
-  mutate(WL_mean = rowMeans(GWL_mmv[ , columns_to_mean], na.rm = TRUE))
-}
-
 #need to aggregate by day for OWASIS
 AFPS_int_SENTEK <- AFPS_int_SENTEK %>% 
   group_by(datetime = format(datetime, "%Y-%m-%d")) %>% 
@@ -84,36 +61,23 @@ AFPS_int_TENSIO<- AFPS_int_TENSIO %>%
   group_by(datetime = as.Date(datetime)) %>% 
   summarise(across(everything(), ~mean(., na.rm = TRUE)))
 
-
-#Second aggregation for OWASIS
-AFPS_int_SENTEK_O <- AFPS_int_SENTEK_O %>% 
-  group_by(datetime = format(datetime, "%Y-%m-%d")) %>% 
-  summarise(across(everything(), ~mean(., na.rm = TRUE)))  
-
-AFPS_TENSIO$datetime <- as.POSIXct(AFPS_TENSIO$datetime, format = "%Y-%m-%d")
+#Correct datetime format
 AFPS_int_SENTEK$datetime <- as.POSIXct(AFPS_int_SENTEK$datetime, format = "%Y-%m-%d")
-AFPS_int_SO$datetime <- as.POSIXct(AFPS_int_SO$datetime, format = "%Y-%m-%d") 
-AFPS_int_TS$datetime <- as.POSIXct(AFPS_int_TS$datetime, format = "%Y-%m-%d")
-AFPS_int_TENSIO$datetime <- as.POSIXct(AFPS_int_TENSIO$datetime, format = "%Y-%m-%d")
-OWASIS_BBB$Date <- as.POSIXct(OWASIS_BBB$Date, format = "%Y-%m-%d")
-
-
 
 #Combined AFPS TENSIO AND SENTEK
 AFPS_int_TS <- bind_cols(AFPS_int_SENTEK[1], AFPS_int_SENTEK[ ,27:28], AFPS_int_TENSIO[ ,2:3], OWASIS_BBB[4])
 
-AFPS_int_SO <- bind_cols(AFPS_int_SENTEK_O[1], AFPS_int_SENTEK_O[ ,27:28], OWASIS_BBB[4])
+#correct datetime format
+AFPS_int_TS$datetime <- as.POSIXct(AFPS_int_TS$datetime, format = "%Y-%m-%d")
 
-AFPS_int_TS_30min <- bind_cols(AFPS_int_SENTEK[1], AFPS_int_SENTEK[ ,27:28], AFPS_int_TENSIO[ ,2:3])
 
-write_rds(AFPS_int_TS, file = "App/AFPS_int_TS.rds")
 
 #PLOTTING
 
 #TENSIO AND SENTEK WITH VOLUME = 60 CM
 ggplot(AFPS_int_TS) +
   geom_line(aes(x = datetime, y = Probe1, color = "SENTEK1"), linewidth = 0.3) +
-  geom_line(aes(x = datetime, y = AFPS_int2, color = "TENSIO2"), linewidth = 0.3) +
+  geom_line(aes(x = datetime, y = AFPS3, color = "TENSIO2"), linewidth = 0.3) +
   geom_line(aes(x = datetime, y = MedianBBB, color = "OWASIS"), linewidth = 0.3) +
   labs(
     title = "AFPS int (SENTEK  VS TENSIO VS OWASIS) depth: 20-60 and unknown OWASIS",
