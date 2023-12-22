9adbeb6e1d01f3c998093aed20f9cd8cce2dd505
marsha727 <marsharoverss@gmail.com>
2023-12-22 14:46:56 +0100

    add datetime

--- Combined file.R
+++ Combined file.R
@@ -19,7 +19,21 @@ WL <- WL %>%
   filter(datetime >= start_date & datetime <= end_date) %>% 
   rename(datetime3 = datetime)
 
-Langeweide <- cbind(AFPS, WL, ET) 
+Tsoil <- Langeweide_data %>% 
+  select(datetime, Tsoil_1_005, Tsoil_1_015, Tsoil_1_025, Tsoil_1_035,
+         Tsoil_1_045, Tsoil_1_055, Tsoil_1_065, Tsoil_1_075, Tsoil_1_085,
+         Tsoil_1_095, Tsoil_1_105, Tsoil_3_005, Tsoil_3_015, Tsoil_3_025, Tsoil_3_035,
+         Tsoil_3_045, Tsoil_3_055, Tsoil_3_065, Tsoil_3_075, Tsoil_3_085,
+         Tsoil_3_095, Tsoil_3_105) %>% 
+  group_by(datetime = format(datetime, "%Y-%m-%d")) %>% 
+  summarise(across(everything(), ~mean(., na.rm = TRUE))) %>% 
+  filter(datetime >= start_date & datetime <= end_date) %>% 
+  rename(datetime4 = datetime)
+  
+
+Langeweide <- cbind(AFPS, WL, ET, Tsoil) 
 
 Langeweide <- Langeweide %>% 
-  select(-c(EF1, EF2, datetime2, datetime3, T))
+  select(-c(EF1, EF2, datetime2, datetime3, T, datetime4))
+
+
