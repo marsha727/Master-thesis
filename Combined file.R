04b874f72398d655d3bfac876f02fd2fcfcd4609
marsha727 <marsharoverss@gmail.com>
2024-01-11 16:24:07 +0100

    Some plots and WL update

--- Combined file.R
+++ Combined file.R
@@ -4,7 +4,9 @@ library(tidyverse)
 ET <- readRDS("App/Langeweide_ET.rds")
 AFPS <- readRDS("App/AFPS_int_TS.rds")
 Langeweide_data <- readRDS("Datasets/LAW_MS_ICOS.RDS")
-WL <- readRDS("Transformed/Langeweide_groundwater_old.rds")
+WL <- readRDS("Transformed/Langeweide_groundwater.rds")
+
+#daily aggregation
 
 start_date <- min(AFPS$datetime)
 end_date <- max(AFPS$datetime)
@@ -29,14 +31,70 @@ Tsoil <- Langeweide_data %>%
   summarise(across(everything(), ~mean(., na.rm = TRUE))) %>% 
   filter(datetime >= start_date & datetime <= end_date) %>% 
   rename(datetime4 = datetime)
+
+NEE <- Langeweide_data %>% 
+  select(datetime, NEE_CO2_MDS2, NEE_CO2_MDS, NEE_CO2) %>% 
+  group_by(datetime = format(datetime, "%Y-%m-%d")) %>% 
+  summarise(NEE_CO2_MDS2 = sum(NEE_CO2_MDS2, na.rm = TRUE),
+            NEE_CO2_MDS = sum(NEE_CO2_MDS, na.rm = TRUE),
+            NEE_CO2 = sum(NEE_CO2, na.rm = TRUE)) %>% 
+  filter(datetime >= start_date & datetime <= end_date) %>% 
+  rename(datetime5 = datetime)
+
   
 
-Langeweide <- cbind(AFPS, WL, ET, Tsoil) 
+Langeweide <- cbind(AFPS, WL, ET, Tsoil, NEE) 
 
 Langeweide <- Langeweide %>% 
-  select(-c(EF1, EF2, datetime2, datetime3, T, datetime4))
+  select(-c(EF1, EF2, datetime2, datetime3, T, datetime4, datetime5))
 
 write_rds(Langeweide, "App/Langeweide_full.rds")
 
 test <- readRDS("App/Langeweide_full.rds")
+
+ggplot(Langeweide) +
+  geom_point(aes(x = Tair, y = NEE_CO2_MDS2, color = WL)) +
+  scale_color_viridis(option = "turbo", trans = "reverse")
+
+ggplot(Langeweide) +
+  geom_point(aes(x = SENTEK1, y = NEE_CO2_MDS2, shape = "SENTEK1", color = WL)) +
+  geom_point(aes(x = SENTEK3, y = NEE_CO2_MDS2, shape = "SENTEK3", color = WL)) +
+  geom_point(aes(x = TENSIO2, y = NEE_CO2_MDS2, shape = "TENSIO2", color = WL)) +
+  geom_point(aes(x = TENSIO3, y = NEE_CO2_MDS2, shape = "TENSIO3", color = WL)) +
+  geom_point(aes(x = OWASIS, y = NEE_CO2_MDS2, shape = "OWASIS", color = WL)) +
+  scale_shape_manual(values = c(16, 17, 25, 15, 22)) +
+  scale_color_viridis(option = "turbo", trans = "reverse") +
+  labs(
+    x = "AFPS [mm]",
+    y = "NEE CO2 [kg day-1 ha-1]"
+  ) +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, size = 1),
+    panel.background = element_rect(fill = "white", color = "black")
+  )
+
+ggplot(Langeweide) +
+  geom_point(aes(x = SENTEK1, y = NEE_CO2_MDS2, color = "SENTEK1")) +
+  geom_point(aes(x = SENTEK3, y = NEE_CO2_MDS2, color = "SENTEK3")) +
+  geom_point(aes(x = TENSIO2, y = NEE_CO2_MDS2, color = "TENSIO2")) +
+  geom_point(aes(x = TENSIO3, y = NEE_CO2_MDS2, color = "TENSIO3")) +
+  geom_point(aes(x = OWASIS, y = NEE_CO2_MDS2, color = "OWASIS")) +
+  scale_shape_manual(values = c(16, 17, 25, 15, 22)) +
+  scale_color_manual(
+    values = c("SENTEK1" = "darkred", "SENTEK3" = "tomato", 
+               "TENSIO2" = "darkblue", "TENSIO3" = "skyblue",
+               "OWASIS" = "darkgreen")
+  ) +
+  labs(
+    x = "AFPS [mm]",
+    y = "NEE CO2 [kg day-1 ha-1]",
+    color = "Legend"
+  ) +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, size = 1),
+    panel.background = element_rect(fill = "white", color = "black")
+  )
+
+
+
 
