8894fbb4218c96dbbc490601b07271affb8880a8
marsha727 <marsharoverss@gmail.com>
2024-01-24 15:27:18 +0100

    some exploration

--- Randomforest.R
+++ Randomforest.R
@@ -1,4 +1,5 @@
 #Random forest
+library(tidyverse)
 library(randomForest)
 library(caret)
 library(e1071)
@@ -7,17 +8,17 @@ library(caTools)
 Langeweide <- readRDS("Langeweide/Statistics_file.rds")
 
 Langeweide <- Langeweide %>% 
-  select(-c(ET, EF, AFPS_mean, datetime, OWASIS)) %>% 
+  select(-c(ET, EF, AFPS_mean, datetime)) %>% 
   na.omit()
 
-model <- randomForest(NEE_CO2_MDS_small ~., data = Langeweide, ntree = 50, do.trace = T)
+model <- randomForest(NEE_CO2_MDS_small ~., data = Langeweide, do.trace = T, importance = TRUE)
 print(model)
 plot(model)
 
 which.min(model$mse)
 sqrt(model$mse[which.min(model$mse)]) 
 
-mtry <- tuneRF(Langeweide[-1], Langeweide$NEE_CO2_MDS_small, stepFactor = 1.5, improve = 0.01, trace = T, plot = T, ntreeTry = 50)
+mtry <- tuneRF(Langeweide[-1], Langeweide$NEE_CO2_MDS_small, stepFactor = 1.5, improve = 0.01, trace = T, plot = T, ntreeTry = 50, mtryStart = 4)
 
 best.m <- mtry[mtry[,2] == min(mtry[,2]), 1]
 
@@ -25,11 +26,13 @@ print(mtry)
 print(best.m)
 
 
-model2 <- randomForest(NEE_CO2_MDS_small~., data = Langeweide, ntree = 100, mtry = 11, importance = TRUE)
+model2 <- randomForest(NEE_CO2_MDS_small~., data = Langeweide, ntree = 50, mtry = 4, importance = TRUE, maxnodes = 25)
+
+model2
 
 print(model2)
 
-importance(model)
-varImpPlot(model)
+importance(model2)
+varImpPlot(model2)
 
 pred1 = predict(model2, type = "prob")
