aae36758f86391ab3abcdd8f1836ea6fe4806fed
marsha727 <marsharoverss@gmail.com>
2024-02-23 16:32:04 +0100

    Final version

--- Soil moisture conversions.R
+++ Soil moisture conversions.R
@@ -21,7 +21,7 @@ SF_value <- function(x){
 SF_Soil_moisture <- Soil_moisture %>% 
   mutate(across(-1:-2, ~SF_value(.)))
 
-#Next step would be to do Daniel's calculations of calibration curve
+#Next step would be to do Daniel van de Craats (WEnR) calculations of calibration curve
 
 #SF corrected for temperature will be calculated first
 #Make a new formula for SF corrected
@@ -50,7 +50,7 @@ for(col in cols_to_normalize){
   SF_n[[col]] <- SF_norm(SF_cor[[col]], max_value)
 }
 
-#Transform to WFPS
+#Transform to WFPS (%)
 WFPS_cali <- function(x){
   ifelse(is.na(x), NA, 0.7033*x+0.2647)
 }
@@ -58,28 +58,8 @@ WFPS_cali <- function(x){
 WFPS_Sentek <- SF_n %>% 
   mutate(across(-1:-2, ~WFPS_cali(.)))
 
-
-#Normalizing the soil moisture
-#Max_SM <- sapply(Soil_moisture, function(x) max(x, na.rm = TRUE))
-#Min_SM <- sapply(Soil_moisture, function(x) min(x, na.rm = TRUE))
-
-#SM_norm <- function(x, max_SM, min_SM){
-  #ifelse(is.na(x), NA, (x - min_SM)/(max_SM - min_SM))
-#}
-
-#cols_to_normalize <- setdiff(names(Soil_moisture), c("datetime", "Tair"))
-
-#SM_n <- Soil_moisture
-
-#for(col in cols_to_normalize){
- # max_SM <- Max_SM[col] 
-  #min_SM <- Min_SM[col]
-  #print(max_SM)
-  #print(min_SM)
-  #SM_n[[col]] <- SM_norm(SM_n[[col]], max_SM, min_SM)
-#}
-
-#calculating the AFPS
+#calculating the AFPS (mm) using the max WFPS (%) measured
+#converted by millimeter by calculating TPS = percentage of pores against pore volume
 Max_values_WFPS <- sapply(WFPS_Sentek, function(x) max(x, na.rm = TRUE))
 AFPS <- function(x, max_value){
   ifelse(is.na(x), NA, (1 - (x / max_value)) * max_value * 100)
@@ -88,7 +68,7 @@ AFPS <- function(x, max_value){
 #To prevent issues with length of the columns i remove the first two rows already
 cols_to_normalize_AFPS <- setdiff(names(WFPS_Sentek), c("datetime", "Tair"))
 
-#For the loop to work i already make a new dataframe for SF normalized
+#For the loop to work i already make a new dataframe for AFPS in mm
 AFPS_mm_SENTEK <- WFPS_Sentek
 
 #this loop apply the formula to each column that i specified earlier
@@ -100,38 +80,7 @@ for(col in cols_to_normalize_AFPS){
   AFPS_mm_SENTEK[[col]] <- AFPS(WFPS_Sentek[[col]], max_value)
 }
 
-#Make a new dataframe that contains SWC, SF and WFPS
-#new_column_names_WFPS <- paste(names(WFPS_Sentek), "WFPS", sep = "_") #Give new colnames to differentiate
-#colnames(WFPS_Sentek) <- new_column_names_WFPS
-#new_column_names_SF <- paste(names(SF_Soil_moisture), "SF", sep = "_")
-#colnames(SF_Soil_moisture) <- new_column_names_SF
-
-#Sentek <- bind_cols(Soil_moisture, SF_Soil_moisture, WFPS_Sentek)
-
-#Sentek <- Sentek %>% #remove double columns
- # select(-datetime_WFPS, -datetime_SF, -Tair_WFPS, -Tair_SF)
-
-#A second version with also the normalized values
-#new_column_names_SMn <- paste(names(SM_n), "N", sep = "_")
-#colnames(SM_n) <- new_column_names_SMn
-#new_column_names_SFn <- paste(names(SF_n), "SF_N", sep = "_")
-#colnames(SF_n) <- new_column_names_SFn
-
-#Sentek_Norm <- bind_cols(Sentek, SM_n, SF_n)
-
-#Sentek_Norm <- Sentek_Norm %>% #remove double columns
-  #select(-datetime_N, -datetime_SF_N, -Tair_N, -Tair_SF_N)
-
-#Ensures the datetime is in correct formating for writing csv
-#Sentek$datetime <- format(Sentek$datetime, format = "%Y-%m-%d %H:%M:%S")
-#Sentek_Norm$datetime <- format(Sentek_Norm$datetime, format = "%Y-%m-%d %H:%M:%S")
-#SM_n$datetime <- format(SM_n$datetime, format = "%Y-%m-%d %H:%M:%S")
 AFPS_mm_SENTEK$datetime <- format(AFPS_mm_SENTEK$datetime, format = "%Y-%m-%d %H:%M:%S")
 
-#Extracting dataset to CSV
-#write.csv2(Sentek, file = "Transformed/Langeweide_Sentek.csv", row.names = FALSE)
-#write.csv2(Sentek_Norm, file = "Transformed/Langeweide_Sentek_normalized.csv", row.names = FALSE)
-#write.csv2(SM_n, file = "Transformed/Langeweide_normalized_SWC.csv", row.names = FALSE)
-
 write.csv2(AFPS_mm_SENTEK, file = "Transformed/Langeweide_Sentek_AFPS.csv", row.names = FALSE)
-test.read <- read.csv2("Transformed/Langeweide_Sentek_AFPS.csv")
+#test.read <- read.csv2("Transformed/Langeweide_Sentek_AFPS.csv")
